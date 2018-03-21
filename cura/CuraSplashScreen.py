55fe3f362a476bf3daef03555f6f06be08999c96
Anthony Markowitz <SunPSoftware>
2018-03-21 13:18:22 -0400

    modified:   cura/CuraSplashScreen.py

--- cura/CuraSplashScreen.py
+++ cura/CuraSplashScreen.py
@@ -64,26 +64,26 @@ class CuraSplashScreen(QSplashScreen):
         if len(version) > 1:
             font.setPixelSize(16)
             painter.setFont(font)
-            painter.setPen(QColor(200, 200, 200, 255))
-            painter.drawText(247, 105, 330 * self._scale, 255 * self._scale, Qt.AlignLeft | Qt.AlignTop, version[1])
-        painter.setPen(QColor(255, 255, 255, 255))
+            painter.setPen(QColor(28, 28, 28, 255))
+            painter.drawText(20, 105, 330 * self._scale, 255 * self._scale, Qt.AlignLeft | Qt.AlignTop, version[1])
+        painter.setPen(QColor(28, 28, 28, 255))
 
         # draw the loading image
         pen = QPen()
         pen.setWidth(6 * self._scale)
         pen.setColor(QColor(32, 166, 219, 255))
         painter.setPen(pen)
-        painter.drawArc(60, 150, 32 * self._scale, 32 * self._scale, self._loading_image_rotation_angle * 16, 300 * 16)
+        painter.drawArc(65, 140, 32 * self._scale, 32 * self._scale, self._loading_image_rotation_angle * 16, 300 * 16)
 
         # draw message text
         if self._current_message:
             font = QFont()  # Using system-default font here
             font.setPixelSize(13)
             pen = QPen()
-            pen.setColor(QColor(255, 255, 255, 255))
+            pen.setColor(QColor(14, 17, 60, 255))
             painter.setPen(pen)
             painter.setFont(font)
-            painter.drawText(100, 128, 170, 64,
+            painter.drawText(20, 150, 170, 64,
                              Qt.AlignLeft | Qt.AlignVCenter | Qt.TextWordWrap,
                              self._current_message)
 
