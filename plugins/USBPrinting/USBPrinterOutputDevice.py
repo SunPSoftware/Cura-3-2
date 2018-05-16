8d76643a0adcc63f3ca885c628ca1384129d7962
Anthony Markowitz <SunPSoftware>
2018-05-16 16:55:39 -0400

    modified:   plugins/USBPrinting/USBPrinterOutputDevice.py

--- plugins/USBPrinting/USBPrinterOutputDevice.py
+++ plugins/USBPrinting/USBPrinterOutputDevice.py
@@ -473,7 +473,7 @@ class USBPrinterOutputDevice(PrinterOutputDevice):
         except serial.SerialTimeoutException:
             Logger.log("w","Serial timeout while writing to serial port, trying again. Command: %s", cmd)
             try:
-                time.sleep(0.5)
+                time.sleep(2)
                 self._serial.write((cmd + "\n").encode())
             except Exception as e:
                 Logger.log("e","Unexpected error while writing serial port %s " % e)
